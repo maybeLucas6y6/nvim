5c7c4e48b8b138a7ef5b2f8cc5d68d143dcf6adc
lpetrovici <lpetrovici@bitdefender.com>
2025-03-14 23:25:15 +0200

    improved telescope

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,10 +6,18 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true, de
 
 vim.keymap.set("n", "J", "mzJ`z", { noremap = true, silent = true, desc = "Join with next row but preserve cursor position" })
 
-vim.keymap.set("n", "<leader-l>", ":cnext<CR>", { noremap = true, silent = true, desc = "Jump to next location in quickfix list (session level)" })
-vim.keymap.set("n", "<leader-h>", ":cprev<CR>", { noremap = true, silent = true, desc = "Jump to previous location in quickfix list (session level)" })
-vim.keymap.set("n", "<leader>k", ":lnext<CR>", { noremap = true, silent = true, desc = "Jump to next location in location list (buffer level)" })
-vim.keymap.set("n", "<leader>j", ":lprev<CR>", { noremap = true, silent = true, desc = "Jump to previous location in location list (buffer level)" })
+-- vim.keymap.set("n", "<leader-l>", ":cnext<CR>", { noremap = true, silent = true, desc = "Jump to next location in quickfix list (session level)" })
+-- vim.keymap.set("n", "<leader-h>", ":cprev<CR>", { noremap = true, silent = true, desc = "Jump to previous location in quickfix list (session level)" })
+-- vim.keymap.set("n", "<leader>k", ":lnext<CR>", { noremap = true, silent = true, desc = "Jump to next location in location list (buffer level)" })
+-- vim.keymap.set("n", "<leader>j", ":lprev<CR>", { noremap = true, silent = true, desc = "Jump to previous location in location list (buffer level)" })
+
+-- vim.keymap.set("n", "<leader>k", vim.diagnostic.goto_prev, { noremap = true, silent = true, desc = "Jump to previous disgnostic" })
+-- vim.keymap.set("n", "<leader>j", vim.diagnostic.goto_next, { noremap = true, silent = true, desc = "Jump to next disgnostic" })
+-- vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { noremap = true, silent = true, desc = "Get symbol definition" })
+-- vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { noremap = true, silent = true, desc = "Get symbol references" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { noremap = true, silent = true, desc = "LSP code action" })
+vim.keymap.set("n", "<leader>bf", vim.lsp.buf.format, { noremap = true, silent = true, desc = "Format buffer" })
+vim.keymap.set("n", "<leader>rr", vim.lsp.buf.rename, { noremap = true, silent = true, desc = "Symbol rename" })
 
 vim.keymap.set("v", "p", [["_dP]], { noremap = true, silent = true, desc = "Prevent paste from overriding your paste buffer" })
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]], { noremap = true, silent = true, desc = "Prevent delete from overriding your paste buffer" })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
     branch = "0.1.x",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        -- "nvim-telescope/telescope-ui-select.nvim",
+        "nvim-telescope/telescope-ui-select.nvim",
         {
             "nvim-telescope/telescope-fzf-native.nvim",
             build = "make"
@@ -17,7 +17,23 @@ return {
         vim.keymap.set("n", "<leader>fb", builtin.buffers, { noremap = true, silent = true, desc = "Telescope buffers" })
         vim.keymap.set("n", "<leader>fh", builtin.help_tags, { noremap = true, silent = true, desc = "Telescope help tags" })
         vim.keymap.set("n", "<leader>fG", builtin.git_files, { noremap = true, silent = true, desc = "Telescope git files" })
+        vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { noremap = true, silent = true, desc = "Telescope symbol references" })
+        vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { noremap = true, silent = true, desc = "Telescope buffer's symbols" })
+        vim.keymap.set("n", "<leader>fS", builtin.lsp_workspace_symbols, { noremap = true, silent = true, desc = "Telescope workspace's symbols" })
+        vim.keymap.set("n", "<leader>fi", builtin.lsp_implementations, { noremap = true, silent = true, desc = "Goto or telescope word's implemtation" })
+        vim.keymap.set("n", "<leader>fd", builtin.lsp_definitions, { noremap = true, silent = true, desc = "Goto or telescope word's definition" })
+        vim.keymap.set("n", "<leader>ft", builtin.lsp_type_definitions, { noremap = true, silent = true, desc = "Goto or telescope word's type definition" })
+        vim.keymap.set("n", "<leader>fD", builtin.diagnostics, { noremap = true, silent = true, desc = "Telescope buffer diagnostics" })
 
+        require("telescope").setup({
+            extensions = {
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown({}),
+                },
+            },
+        })
+
+        require("telescope").load_extension("ui-select")
         require("telescope").load_extension("fzf")
     end,
 }
