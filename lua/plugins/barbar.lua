915cb825abe58bc183d9839db202c169f10a4a5c
lpetrovici <lpetrovici@bitdefender.com>
2025-03-14 17:17:50 +0200

    barbar

---
+++ lua/plugins/barbar.lua
@@ -0,0 +1,22 @@
+return {
+    "romgrk/barbar.nvim",
+    dependencies = {
+        -- TODO
+        -- "lewis6991/gitsigns.nvim",
+        "nvim-tree/nvim-web-devicons",
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+        animation = false,
+        sidebar_filetypes = {
+            NvimTree = true,
+        },
+    },
+    keys = {
+        { mode = { "n" }, "<A-,>", vim.cmd.BufferPrevious,  noremap = true, silent = true, desc = "Switch to previous buffer" },
+        { mode = { "n" }, "<A-.>", vim.cmd.BufferNext,  noremap = true, silent = true, desc = "Switch to next buffer" },
+        { mode = { "n" }, "<A-<>", vim.cmd.BufferMovePrevious,  noremap = true, silent = true, desc = "Move buffer to left" },
+        { mode = { "n" }, "<A->>", vim.cmd.BufferMoveNext,  noremap = true, silent = true, desc = "Move buffer to right" },
+        { mode = { "n" }, "<A-c>", vim.cmd.BufferClose,  noremap = true, silent = true, desc = "Close current buffer" },
+    }
+}
