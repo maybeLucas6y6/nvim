6e8d9d3e7a28987e128987ba246a6dfe57b6d42f
lpetrovici <lpetrovici@bitdefender.com>
2025-03-14 18:08:14 +0200

    lazygit

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,17 @@
+return {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    keys = {
+        { mode = { "n" }, "<Leader>G", vim.cmd.LazyGit,  noremap = true, silent = true, desc = "Open lazygit" },
+    }
+}
