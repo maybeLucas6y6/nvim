0643565b21cf6c6c7ac822696a4c576ea9bd9432
lpetrovici <lpetrovici@bitdefender.com>
2025-03-14 20:43:56 +0200

    fix nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,9 +4,14 @@ return {
         { mode = { "n" }, "<C-n>", vim.cmd.NvimTreeToggle,  noremap = true, silent = true, desc = "Toggle nvim-tree" },
         { mode = { "n" }, "<Leader>e", vim.cmd.NvimTreeFocus,  noremap = true, silent = true, desc = "Change focus to nvim-tree" },
     },
-    config = function()
-        require("nvim-tree").setup({
-            hijack_cursor = false,
-        })
-    end,
+    opts = {
+        hijack_cursor = false,
+        view = {
+            adaptive_size = true,
+            preserve_window_proportions = true,
+            width = {
+                padding = 2,
+            },
+        },
+    }
 }
