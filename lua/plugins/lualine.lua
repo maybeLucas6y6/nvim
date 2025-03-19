3a6c66ca37b685a31bcc77766e4d3fa460b304bb
lpetrovici <lpetrovici@bitdefender.com>
2025-03-19 15:19:10 +0200

    improved lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,13 +3,23 @@ return {
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
-    config = function()
-        require("lualine").setup({
-            options = {
-                theme = "auto",
-                component_separators = "|",
-                section_separators = { left = "", right = "" },
-            },
-        })
-    end,
+    opts = {
+        options = {
+            theme = "auto",
+            component_separators = "|",
+            section_separators = { left = "", right = "" },
+        },
+        sections = {
+            lualine_a = { "mode" },
+            lualine_b = { "branch", "diff", "diagnostics" },
+            -- lualine_c = { require("nvim-navic").get_location, cond = require("nvim-navic").is_available },
+            lualine_x = { "encoding", "fileformat", "filetype" },
+            lualine_y = { "progress" },
+            lualine_z = { "location" }
+        },
+        extensions = {
+            "nvim-tree",
+            "nvim-dap-ui",
+        }
+    },
 }
