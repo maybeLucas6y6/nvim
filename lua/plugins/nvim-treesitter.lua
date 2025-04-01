acf3b6e3622845d49f1f4ca580901ff20962a4d7
lpetrovici <lpetrovici@bitdefender.com>
2025-04-01 11:49:38 +0300

    fix docs formatting

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -4,7 +4,15 @@ return {
     config = function ()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
-            ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown" },
+            ensure_installed = {
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                "markdown",
+                "markdown_inline",
+                "gitignore",
+            },
             sync_install = false,
             auto_install = true,
             highlight = { enable = true },
