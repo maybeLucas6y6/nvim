7c658b9a3e747c655146b264849c9a05e3674e2d
lpetrovici <lpetrovici@bitdefender.com>
2025-09-26 10:28:49 +0300

    fix lspconfig

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -8,24 +8,26 @@ return {
         })
 
         local navic = require("nvim-navic")
-
         local blink = require("blink.cmp")
-        -- Servers: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.txt
-        local servers = { "lua_ls", "clangd" }
-        local base_capabilities = vim.lsp.protocol.make_client_capabilities()
 
+        -- Servers: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.txt
+        -- or :h lspconfig-all
+        local servers = { "lua_ls", "clangd", "pyright" }
+
+        local base_capabilities = vim.lsp.protocol.make_client_capabilities()
         local capabilities = blink.get_lsp_capabilities(base_capabilities)
 
         for _, server_name in ipairs(servers) do
             -- TODO: currently disabled. Broken when migrated to 0.11.4
-            -- vim.lsp.config(server_name).setup({
-            --     on_attach = function(client, bufnr)
-            --         if client.server_capabilities.documentSymbolProvider then
-            --             navic.attach(client, bufnr)
-            --         end
-            --     end,
-            --     capabilities = capabilities,
-            -- })
+            vim.lsp.config(server_name, {
+                on_attach = function(client, bufnr)
+                    if client.server_capabilities.documentSymbolProvider then
+                        navic.attach(client, bufnr)
+                    end
+                end,
+                capabilities = capabilities,
+            })
+            vim.lsp.enable(server_name)
         end
     end,
 }
