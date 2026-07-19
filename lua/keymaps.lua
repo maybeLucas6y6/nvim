8d001a4f402076efcef132894872667eb6dfc357
maybeLucas6y6 <petro.luca2004@gmail.com>
2026-07-19 22:17:19 +0300

    take out inlay hint keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -18,9 +18,5 @@ vim.keymap.set("n", "<leader>lr", function ()
     vim.opt.wrap = not vim.opt.wrap:get()
 end, { noremap = true, silent = true, desc = "Toggle line wrap" })
 
-vim.keymap.set("n", "<leader>ih", function ()
-    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-end, { noremap = true, silent = true, desc = "Toggle inlay hint" })
-
 vim.api.nvim_set_keymap("n", "<leader>fj", ":%!jq '.'<CR>", { noremap = true, silent = true, desc = "Format buffer using jq (for json)" })
 vim.api.nvim_set_keymap("n", "<leader>fy", ":%!yq '.'<CR>", { noremap = true, silent = true, desc = "Format buffer using yq (for yaml)" })
